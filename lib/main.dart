8c8ee327c3e3c3122d1981c17bf3a8bd41557ffe
Guillo7799 <guillermo.rivera@epn.edu.ec>
2021-08-17 17:54:10 -0500

    Números primos hechos en flutter - GR

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Números Primos'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _a = 2;
+  void _generatePrime() {
+    int cont = 0;
+    int num = 100;
+    for (var i = 1; i <= num; i++) {
+      if (num % i == 0) {
+        cont++;
+      }
+    }
+    if (cont == 2) {
+      setState(() {
+        _a = num;
+      });
+    } else {
+      _generatePrime();
+    }
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Los números son:',
+            ),
+            Text(
+              '$_a',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _generatePrime,
+        tooltip: 'Numero',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
